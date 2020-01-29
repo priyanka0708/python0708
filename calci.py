85288f28ca61060881f684cd3c7849ee73f44fda
priyanka0708 <60418732+priyanka0708@users.noreply.github.com>
2020-01-29 10:56:47 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return(x+y)
+def sub(x,y):
+    return(x-y)
+def div(x,y):
+    return(x/y)
+def mul(x,y):
+    return(x*y)
+print("select operation")
+print("1.Add")
+print("2.Sub")
+print("3.Mul")
+print("4.Div")
